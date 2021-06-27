4ddff4c44bcbc9ea87f23e374061e9e59845cbfa
Sam Bennion <sambennion2006@gmail.com>
2021-06-27 10:58:04 +0100

    frumpy

---
+++ board.py
@@ -0,0 +1,13 @@
+class board():
+    test = "glommamamama"
+
+    def __init__(self):
+        self.test2 = None
+
+    
+    def set(self, data):
+        self.test2 = data
+    
+
+    def get(self):
+        return self.test2
--- foomy.py
+++ foomy.py
@@ -1,3 +1,29 @@
-print("Foomy FUN fun!")
-print("Fooomoo no foofoo :-(")
-print("Final test")
+import socket
+import time
+import board
+import pickle
+
+serverIP = "192.168.1.161"
+serverPort = "60001"
+clientIP = None
+clientPort = None
+
+myboard = board()
+myboard.set("tony")
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', serverPort))
+sock.blocking(False)
+
+while (True):
+    
+    try:
+        (data, addr) = sock.recvfrom(65536)
+    except:
+        pass
+
+    if len(data) > 0:
+        print("Got it *-*")
+    
+    sock.sendto(pickle.dumps(myboard), ("192.168.1.179", 60001))
